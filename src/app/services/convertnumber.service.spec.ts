d906b5b3d79606de1c4bade6c01b1329a4011d03
Hector Guerra <hector.guerra@udea.edu.co>
2020-12-20 12:52:36 -0500

    cambio de algunas pruebas

--- src/app/services/convertnumber.service.spec.ts
+++ src/app/services/convertnumber.service.spec.ts
@@ -13,49 +13,40 @@ describe('ConvertnumberService', () => {
     expect(service).toBeTruthy();
   });
 
-  it("Number 1 -> I", () => {
+  it("number one", () => {
     expect(service.getRomanNumber(1)).toEqual('I');
   });
 
-  it("Number 5 -> V", () => {
+  it("number 5", () => {
     expect(service.getRomanNumber(5)).toEqual('V');
   });
 
-  it("Number 10 -> X", () => {
+  it("number ten", () => {
     expect(service.getRomanNumber(10)).toEqual('X');
   });
 
-  it("Number 50 -> L", () => {
+  it("number fifty", () => {
     expect(service.getRomanNumber(50)).toEqual('L');
   });
 
-  it("Number 100 -> C", () => {
+  it("number hundred", () => {
     expect(service.getRomanNumber(100)).toEqual('C');
   });
 
-  it("Number 500 -> D", () => {
+  it("number five hundred", () => {
     expect(service.getRomanNumber(500)).toEqual('D');
   });
 
-  it("Number 1000 -> M", () => {
+  it("number thousand", () => {
     expect(service.getRomanNumber(1000)).toEqual('M');
   });
 
-  it("Number 4 -> IV", () => {
+  it("number four", () => {
     expect(service.getRomanNumber(4)).toEqual('IV');
   });
 
-  it("Number 499 -> CDXCIX", () => {
+  it("number four hundred ninety nine", () => {
     expect(service.getRomanNumber(499)).toEqual('CDXCIX');
   });
-
-  it("Number 27 -> XXVII", () => {
-    expect(service.getRomanNumber(27)).toEqual('XXVII');
-  });
-
-  it("Number 54 -> LIV", () => {
-    expect(service.getRomanNumber(54)).toEqual('LIV');
-  });
-
 
 });
